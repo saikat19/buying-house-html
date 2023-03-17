db93656349b3f02c2d7c3a05194789b59a069cb3
Shaikot Ahmed <shaikot@bitmascot.com>
2023-03-17 09:40:39 +0600

    home responsive done

--- js/theme.js
+++ js/theme.js
@@ -269,7 +269,33 @@
         infinite: true,
         speed: 300,
         slidesToShow: 4,
-        adaptiveHeight: true
+        adaptiveHeight: true,
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+              }
+            },
+            {
+              breakpoint: 991,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 650,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
       });
 
 
